176883122f1ab0452c5f016f52d1cd8d82833f14
Nate Kupp <nate@elementl.com>
2020-01-02 12:54:48 -0800

    [dagster-airflow] Split apart test_factory.py
    Test Plan: unit
    
    Differential Revision: https://dagster.phacility.com/D1767

---
+++ python_modules/dagster-airflow/dagster_airflow_tests/test_factory/test_kubernetes.py
@@ -0,0 +1,74 @@
+import os
+import uuid
+
+import pytest
+from airflow.exceptions import AirflowException
+from airflow.utils import timezone
+from dagster_airflow.factory import make_airflow_dag_kubernetized_for_handle
+from dagster_airflow.test_fixtures import (  # pylint: disable=unused-import
+    dagster_airflow_k8s_operator_pipeline,
+    execute_tasks_in_dag,
+    get_dagster_docker_image,
+)
+from dagster_airflow_tests.conftest import environments_path
+from dagster_airflow_tests.marks import nettest
+
+from dagster import ExecutionTargetHandle
+from dagster.utils import load_yaml_from_glob_list
+
+from .utils import validate_pipeline_execution
+
+
+@nettest
+class TestExecuteDagKubernetizedS3Storage(object):
+    pipeline_name = 'demo_pipeline'
+    handle = ExecutionTargetHandle.for_pipeline_module('test_pipelines', pipeline_name)
+    environment_yaml = [
+        os.path.join(environments_path, 'env.yaml'),
+        os.path.join(environments_path, 'env_s3.yaml'),
+    ]
+    run_id = str(uuid.uuid4())
+
+    # pylint: disable=redefined-outer-name
+    def test_execute_dag_kubernetized(self, dagster_airflow_k8s_operator_pipeline):
+        validate_pipeline_execution(dagster_airflow_k8s_operator_pipeline)
+
+
+@nettest
+class TestExecuteDagKubernetizedGCSStorage(object):
+    pipeline_name = 'demo_pipeline_gcs'
+    handle = ExecutionTargetHandle.for_pipeline_module('test_pipelines', pipeline_name)
+    environment_yaml = [
+        os.path.join(environments_path, 'env.yaml'),
+        os.path.join(environments_path, 'env_gcs.yaml'),
+    ]
+    run_id = str(uuid.uuid4())
+
+    # pylint: disable=redefined-outer-name
+    def test_execute_dag_kubernetized(self, dagster_airflow_k8s_operator_pipeline):
+        validate_pipeline_execution(dagster_airflow_k8s_operator_pipeline)
+
+
+def test_error_dag_k8s():
+    pipeline_name = 'demo_error_pipeline'
+    handle = ExecutionTargetHandle.for_pipeline_module('test_pipelines', pipeline_name)
+    environment_yaml = [
+        os.path.join(environments_path, 'env_s3.yaml'),
+    ]
+    environment_dict = load_yaml_from_glob_list(environment_yaml)
+
+    run_id = str(uuid.uuid4())
+    execution_date = timezone.utcnow()
+
+    dag, tasks = make_airflow_dag_kubernetized_for_handle(
+        handle=handle,
+        pipeline_name=pipeline_name,
+        image=get_dagster_docker_image(),
+        namespace='default',
+        environment_dict=environment_dict,
+    )
+
+    with pytest.raises(AirflowException) as exc_info:
+        execute_tasks_in_dag(dag, tasks, run_id, execution_date)
+
+    assert 'Exception: Unusual error' in str(exc_info.value)
